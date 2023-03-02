d179173fd03d08786e69af94efa85f25d69ce2d1
pheonixeye <drkazgm@gmail.com>
2023-03-02 11:58:56 +0200

    initial commit

---
+++ lib/magic_buffer.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library magic_buffer;
+
+export 'src/magic_buffer_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
